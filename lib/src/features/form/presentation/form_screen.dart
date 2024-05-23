cdde559d2fc5724ca5eb241cfac81e6deb766a6a
dome-sieb <sieberdominique10@gmail.com>
2024-05-23 14:04:41 +0200

    Validation PW und mail

--- lib/src/features/form/presentation/form_screen.dart
+++ lib/src/features/form/presentation/form_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 class FormScreen extends StatelessWidget {
@@ -10,19 +12,20 @@ class FormScreen extends StatelessWidget {
   // Methoden
   @override
   Widget build(BuildContext context) {
+    var textFormField = TextFormField(
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        label: Text("Email"),
+      ),
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      validator: validateEmail,
+    );
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(32.0),
         child: Form(
           child: Column(children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-                label: Text("Email"),
-              ),
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              validator: validateEmail,
-            ),
+            textFormField,
             const SizedBox(height: 8),
             TextFormField(
               decoration: const InputDecoration(
@@ -43,13 +46,43 @@ class FormScreen extends StatelessWidget {
     );
   }
 
-  String? validateEmail(String? input) {
-    // TODO: implementiere hier die Logik, die im Task Sheet beschrieben ist
-    return null;
+  bool validateEmail(String? input) {
+    //bool validateEmail(String email) {
+    final RegExp emailRegex =
+        RegExp(r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$');
+    final bool isValidEmail = emailRegex.hasMatch(email);
+
+    if (!isValidEmail) {
+      return false;
+    }
+
+    var email;
+    final List<String> emailParts = email.split('@');
+    final String domain = emailParts[1];
+
+    if (domain.endsWith('.com') || domain.endsWith('.de')) {
+      return true;
+    }
+
+    return false;
   }
 
   String? validatePw(String? input) {
-    // TODO: implementiere hier die Logik, die im Task Sheet beschrieben ist
+    bool isValidPassword(String password) {
+      if (password.length < 6 || password.length > 22) {
+        return false;
+      }
+      return true;
+    }
+
+    print("Please enter a password to check its validity:");
+    var inputPassword = stdin.readLineSync();
+    if (isValidPassword(inputPassword!)) {
+      print("The password is valid.");
+    } else {
+      print(
+          "The password is not valid. It must be between 6 and 22 characters long.");
+    }
     return null;
   }
 }
